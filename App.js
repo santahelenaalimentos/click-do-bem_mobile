aebc447dadd3cca3cb71f8d2781f04454bc0ea1e
Danilo Moura <danilomourag@gmail.com>
2018-09-24 17:13:54 -0300

    Add NativeBase

--- App.js
+++ App.js
@@ -8,6 +8,13 @@ export default class App extends React.Component {
     isLoadingComplete: false,
   };
 
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+  }  
+
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (

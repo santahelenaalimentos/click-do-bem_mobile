6f8b5f664e552b3d350a38e9c86bfef2a38d1146
Danilo Moura <danilomourag@gmail.com>
2018-09-26 11:56:09 -0300

    Create login screen prototype

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,184 +1,102 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
-  View,
+  View
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
+import {
+  Button,
+  Item,
+  Label,
+  Input,
+  Content
+} from 'native-base';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={require('../assets/images/sh-logo.png')}
-              style={styles.welcomeImage}
-            />
-          </View>
+  constructor(props){
+    super(props);
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
-      </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
+    this.state = {
+      login: '999.999.999-99',
+      password: '*******',
     }
   }
 
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <View style={styles.halfHeight}>
+          <Image
+            source={require('../assets/images/sh-logo.png')}
+            style={styles.welcomeImage}
+          /> 
+        </View>
 
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+        <Content contentContainerStyle={styles.halfHeight}>
+          <Item floatingLabel style={{width: '80%'}}>
+            <Label style={{color: '#fff'}}>Username</Label>
+            <Input  style={styles.credentialsInput}/>
+          </Item>
+          <Item floatingLabel style={{width: '80%'}}>
+            <Label style={{color: '#fff'}}>Password</Label>
+            <Input  style={styles.credentialsInput}/>
+          </Item>
+
+          <View style={styles.buttonContainer}>
+            <Button block primary style={styles.signinButton}>
+              <Text style={styles.buttonText}>Entrar</Text>
+            </Button>
+            <Button block success style={styles.signupButton}>
+              <Text style={styles.buttonText}>Cadastrar</Text>
+            </Button>
+          </View>
+        </Content>
+
+      </View>
     );
-  };
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
+  halfHeight: {
+    flex: .5,
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
+    justifyContent: 'center'
   },
   welcomeImage: {
-    width: 200,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
+    width: 300,
+    height: 200,
+    resizeMode: 'contain'
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
+  credentialsInput: {
+    width: '85%',
+    height: 50,
+    color: '#333',
   },
-  homeScreenFilename: {
-    marginVertical: 7,
+  buttonContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 15
   },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
+  signinButton: {
+    marginRight: 10,
+    width: '20%'
   },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
+  signupButton: {
+    width: '20%'
   },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  buttonText: {
+    color: '#fff'
+  }
+  
 });
